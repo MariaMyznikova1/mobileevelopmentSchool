feee9fc891ac4f46d7579c0f708d5c29b816a867
Maria Myznikova <mmyznikovaa@yandex.com>
2021-03-17 19:42:10 +0700

    Initial commit

---
+++ MobileDevelopmentSchool/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  MobileDevelopmentSchool
+//
+//  Created by Maria Myznikova on 15.03.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        view.alpha = 0.5
+        view.backgroundColor = .black
+        
+        self.view.backgroundColor = UIColor(red: 0.898, green: 0.898, blue: 0.898, alpha: 1)
+
+
+        
+    //Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
